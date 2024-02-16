20e45b850fba1d17e3d341283fdf5b4dee47fc60
CVedaReddy <157104274+CVedaReddy@users.noreply.github.com>
2024-02-16 16:10:01 +0530

    Update 23mm01007_setB_q4.c

--- 23mm01007_setB_q4.c
+++ 23mm01007_setB_q4.c
@@ -4,39 +4,30 @@ int main()
     int num,i,j,p,k,count=0;
     printf("Enter the number:");
     scanf("%d",num);
-    for(i=4;i<=num;i++)
+    for(i=4;i<=num/2;i++)
     {
+        count=0;
         for(j=2;j<i;j++)
         {
             if((i%j)==0)
             {
-                break;
-
-            }
-            else 
-            {
-            continue;
-        }
-        if(j!= 1)
-        {
-            p=num-i;
-            for(k=2;k<p;k++)
-            {
-                if((p%k)==0)
-                {
-                    break;
-
-                }
-                if(k !=p && p>=i)
-                {
-                    printf("%d \n ",p);
-                    count ++;
-                }}}
-                if(count ==0)
-                printf("The given numbers cannot be expressed as sum of composite numbers");
-                }
-                return 0;
+               count ++;
             }
         }
+p=0;
+for(k=2;k<(num-i);k++)
+{
+if((num-i)%k ==0)
+{
+p++;
+}
+}
+if( count!=0 && p!=0 && i != (num-i))
+{
+printf("%d %d \n ",i,num-i);
+    }}
+return 0;
+}
+        
 
         
